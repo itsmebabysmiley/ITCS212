33f5a84eb65afda996989d0e440e5ac271cc96d8
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2021-02-22 16:20:42 +0700

    add contect cuz I forgot hhh

--- week6/q2/6288103_sec2_q2.js
+++ week6/q2/6288103_sec2_q2.js
@@ -1,1 +1,28 @@
 // It just a node modules(express and nodenom)
+var path = require('path');
+const express = require('express');
+const expressApp = express();
+
+
+expressApp.get('/', (req, res) => {
+    console.log("Req at:"+ req.url);
+    res.status(200).send(' Hello world in plain text');
+    res.end();
+});
+expressApp.get('/th', (req, res) => {
+    console.log("Req at:"+ req.url);
+    res.setHeader("Content-Type","text/html; charset=utf-8;");
+    res.status(200).sendFile(path.join(__dirname,'/greeting_th.html'));
+
+});
+expressApp.get('/cn', (req, res) => {
+    console.log("Req at:"+ req.url);
+    res.setHeader("Content-Type","text/html; charset=utf-8;");
+    res.status(200).sendFile(path.join(__dirname,'/greeting_cn.html'));
+
+});
+
+//sever start on port 3030
+expressApp.listen(3030, function(){
+    console.log("listening on port 3030");
+});
