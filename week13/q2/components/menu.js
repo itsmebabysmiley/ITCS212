f389951651fd1c7957b9c5cbc26077e679116d77
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2021-04-14 12:20:00 +0700

    fix css

--- week13/q2/components/menu.js
+++ week13/q2/components/menu.js
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from 'styled-components';
-const Mydiv = styled.div`
+const MyRow = styled.div`
 display:flex;
 flex-direction: row wrap;
+flex-wrap: wrap;
 justify-content: center;`
-const Myform = styled.div`
+const MyColum = styled.div`
 display:flex;
-flex-direction: column;
+`
+const Myform = styled.form`
+margin: 35px;
+`
+const Content = styled.div`
+display:flex;
+flex-flow: column wrap;
 justify-content: center;
 margin: 30px;
+padding: 10px;
 `
+
+
 class Menu extends React.Component {
     constructor(props) {
         super(props);
@@ -26,7 +36,7 @@ class Menu extends React.Component {
       }
     
       handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        // alert('A name was submitted: ' + this.state.value);
         var APi_key = "c425f3e92397425fa0ca18b93364dbac";
         let rooturl =
         "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
@@ -48,15 +58,23 @@ class Menu extends React.Component {
       render() {
         return (
         <div>
-            <h1>Explore Menu</h1>
-            <form onSubmit={this.handleSubmit}>
-            <label>
-              Are you looking for food? 
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        <Results results = {this.state.data}/>
+          <MyRow>
+            <MyColum>
+              <h1>Explore Menu</h1>
+            </MyColum>
+            <MyColum>
+              <Myform onSubmit={this.handleSubmit}>
+                <label>
+                  Are you looking for food? 
+                  <input name='food' type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <button type="submit" value="Submit">Go</button>
+              </Myform>
+            </MyColum>
+          </MyRow>
+          
+          <Results results = {this.state.data}/>
+
         </div>
         );
       }
@@ -66,17 +84,21 @@ export default Menu;
 class Results extends React.Component{
     render(){
         return (
-            <div>
+            <MyRow>
                 {this.props.results && this.props.results.map(data =>{
                     return (
-                        <div>
-                            <img src={data.image} alt="Girl in a jacket" width="312" height="231"></img>
+                        <Content>
+                          <div>
+                            <img src={data.image} alt="..." width="312" height="231"></img>
+                          </div>
+                          <div>
                             <p id={data.id}> {data.title}</p>
-                        </div> 
+                          </div>
+                        </Content> 
                     )
                 })
                 }
-            </div>
+            </MyRow>
             )
     }
 }
